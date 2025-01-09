143a0b56cc06c44d8c1fba4b7b30c841521a5723
nox <mariam.arroyo@gmail.com>
2025-01-09 11:19:59 +0000

    modifications to so_long.h

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,7 +6,7 @@
 /*   By: amarroyo <amarroyo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 08:51:40 by amarroyo          #+#    #+#             */
-/*   Updated: 2025/01/09 09:55:31 by amarroyo         ###   ########.fr       */
+/*   Updated: 2025/01/09 11:19:06 by amarroyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,43 @@
 # include "ft_printf.h"
 # include "MLX42.h"
 
+// Map elements
+# define WALL '1'
+# define EMPTY '0'
+# define PLAYER 'P'
+# define EXIT 'E'
+# define COLLECTIBLE 'C'
+
+// Error codes
+# define ERR_FILE_OPEN 1
+# define ERR_FILE_READ 2
+# define ERR_INVALID_MAP 3
+# define ERR_MALLOC 4
+
+// Tile size in pixels (industry standard)
+# define TILE_SIZE 32
+
 // Structure(s)
+/* Map struture*/
+typedef struct s_map
+{
+	char		**grid;			// 2D array for the map layout
+	uint32_t	width;			// Width of the map (columns)
+	uint32_t	height;			// Height of the map (rows)
+	int			player_x;		// Player's X position
+	int			player_y;		// Player's Y position
+	uint32_t	collectibles;	// Count of collectibles
+	uint32_t	exits;			// Number of exits
+	uint32_t	players;		// Number of players (should be 1)
+}				t_map;
+
+/* game Structure*/
+typedef struct s_game
+{
+	t_map		*map;			// Pointer to the map structure
+	mlx_t		*mlx;			// MLX42 instance (includes the window)
+	uint32_t	moves;			// Number of moves made by the player
+}				t_game;
 
 // Functions
 

ff64b15b407f95a898fce2f091fd6030a8fd74ce
nox <mariam.arroyo@gmail.com>
2025-01-09 15:54:56 +0000

    so_long.h modified: no longer needs the paths in the includes

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,23 +6,23 @@
 /*   By: amarroyo <amarroyo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 08:51:40 by amarroyo          #+#    #+#             */
-/*   Updated: 2025/01/09 11:19:06 by amarroyo         ###   ########.fr       */
+/*   Updated: 2025/01/09 15:52:02 by amarroyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <fcntl.h>		// open
+# include <fcntl.h>			// open
 # include <unistd.h>		// close - read - write
 # include <stdlib.h>		// malloc - free -exit
-# include <stdio.h>		// perror
-# include <errno.h>		// perror
+# include <stdio.h>			// perror
+# include <errno.h>			// perror
 # include <string.h>		// strerror
 # include "libft.h"
 # include "get_next_line.h"
 # include "ft_printf.h"
-# include "MLX42.h"
+# include "MLX42/MLX42.h"
 
 // Map elements
 # define WALL '1'
@@ -63,5 +63,7 @@ typedef struct s_game
 }				t_game;
 
 // Functions
+int	ft_check_extension(const char *file_path);
+int main(int argc, char **argv);
 
 #endif

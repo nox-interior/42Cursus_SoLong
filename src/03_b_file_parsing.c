1040799a3d5a86a9e7264837d65bbd862d6acdfa
nox <mariam.arroyo@gmail.com>
2025-01-15 12:21:13 +0000

    modified ft_parse_map, no need to check i f map is empty at the end. Already done by ft_validate_map

--- src/03_b_file_parsing.c
+++ src/03_b_file_parsing.c
@@ -6,7 +6,7 @@
 /*   By: amarroyo <amarroyo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 11:50:54 by amarroyo          #+#    #+#             */
-/*   Updated: 2025/01/15 12:03:27 by amarroyo         ###   ########.fr       */
+/*   Updated: 2025/01/15 12:19:15 by amarroyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,5 @@ t_error	ft_parse_map(const char *file_path, t_map *map)
 	error = ft_validate_map(map);
 	if (error != ERR_NONE)
 		return (ft_handle_map_error(error, map, fd));
-	// if (map->height == 0)
-	// 	return (ft_handle_map_error(ERR_MAP_INVALID, map, fd));
 	return (ERR_NONE);
 }

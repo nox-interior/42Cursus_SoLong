5bb9eb89439b2cf1351e36fadc335987f1706c1d
nox <mariam.arroyo@gmail.com>
2025-01-15 12:13:01 +0000

    changed name form map_validation to extension_validation for modularity

---
+++ src/01_extension_validation.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   01_extension_validation.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarroyo <amarroyo@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 08:46:56 by amarroyo          #+#    #+#             */
+/*   Updated: 2025/01/15 11:46:51 by amarroyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+// File handling
+t_error	ft_check_extension(const char *str)
+{
+	int		i;
+
+	i = 0;
+	if (! str || ft_strlen(str) < 5)
+		return (ERR_FILE_PATH_INVALID);
+	while (str[i] != '\0')
+		i++;
+	i--;
+	if (str[i] != 'r' || str[i - 1] != 'e' || str[i - 2] != 'b'
+		|| str[i - 3] != '.')
+		return (ERR_EXTENSION_INVALID);
+	return (ERR_NONE);
+}

714e1cb0a8366e9e5e415e52273a80938d9dd9c8
nox <mariam.arroyo@gmail.com>
2025-03-18 16:11:24 +0000

    solved at last the empty line at EOFgit add src/03_b_file_parsing.c !

--- src/03_b_file_parsing.c
+++ src/03_b_file_parsing.c
@@ -6,7 +6,7 @@
 /*   By: amarroyo <amarroyo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 11:50:54 by amarroyo          #+#    #+#             */
-/*   Updated: 2025/03/17 16:33:01 by amarroyo         ###   ########.fr       */
+/*   Updated: 2025/03/18 16:08:44 by amarroyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,19 +38,35 @@ static t_error	ft_free_map_error(t_error error, t_map *map, int fd)
 	return (error);
 }
 
-t_error	ft_parse_map(const char *file_path, t_map *map, t_winconfig *winconfig)
+static t_error	ft_check_no_trailing_newline(const char *file_path)
 {
 	int		fd;
-	t_error	error;
+	char	buf[1024];
+	ssize_t	n;
+	char	last;
 
-	error = ft_check_extension(file_path);
-	if (error != ERR_NONE)
-		return (error);
-	ft_init_map(map);
-	error = ft_open_and_allocate(file_path, map, winconfig, &fd);
-	if (error != ERR_NONE)
-		return (error);
-	error = ft_read_map_lines(fd, map, winconfig);
+	fd = open(file_path, O_RDONLY);
+	if (fd < 0)
+		return (ERR_OPEN_FILE);
+	last = '\0';
+	while (1)
+	{
+		n = read(fd, buf, 1024);
+		if (n <= 0)
+			break ;
+		last = buf[n - 1];
+	}
+	close(fd);
+	if (n < 0)
+		return (ERR_READ_FILE);
+	if (last == '\n')
+		return (ERR_MAP_INVALID);
+	return (ERR_NONE);
+}
+
+static t_error	ft_finish_parse(t_map *map, t_winconfig *winconfig,
+	int fd, t_error error)
+{
 	close(fd);
 	if (error != ERR_NONE)
 		return (ft_free_map_error(error, map, fd));
@@ -64,4 +80,23 @@ t_error	ft_parse_map(const char *file_path, t_map *map, t_winconfig *winconfig)
 	if (error != ERR_NONE)
 		return (ft_free_map_error(error, map, fd));
 	return (ERR_NONE);
+}
+
+t_error	ft_parse_map(const char *file_path, t_map *map, t_winconfig *winconfig)
+{
+	int		fd;
+	t_error	error;
+
+	error = ft_check_extension(file_path);
+	if (error != ERR_NONE)
+		return (error);
+	error = ft_check_no_trailing_newline(file_path);
+	if (error != ERR_NONE)
+		return (error);
+	ft_init_map(map);
+	error = ft_open_and_allocate(file_path, map, winconfig, &fd);
+	if (error != ERR_NONE)
+		return (error);
+	error = ft_read_map_lines(fd, map, winconfig);
+	return (ft_finish_parse(map, winconfig, fd, error));
 }

a591df5ba1cbccfd8e1e44716d40a8dc4d81bc14
nox <mariam.arroyo@gmail.com>
2025-03-18 16:19:52 +0000

    removed unused fucntion for checking extension

--- src/01_extension_validation.c
+++ src/01_extension_validation.c
@@ -6,7 +6,7 @@
 /*   By: amarroyo <amarroyo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 08:46:56 by amarroyo          #+#    #+#             */
-/*   Updated: 2025/03/17 13:15:06 by amarroyo         ###   ########.fr       */
+/*   Updated: 2025/03/18 16:18:25 by amarroyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,3 @@ t_error	ft_check_extension(const char *str)
 		return (ERR_EXTENSION_INVALID);
 	return (ERR_NONE);
 }
-
-// t_error	ft_check_extension(const char *str)
-// {
-// 	int	len;
-
-// 	if (!str || ft_strlen(str) < 5)
-// 		return (ERR_FILE_PATH_INVALID);
-// 	len = ft_strlen(str);
-// 	if (str[len - 4] == '.' && str[len - 5] == '/')
-// 		return (ERR_FILE_PATH_INVALID);
-// 	if (str[len - 1] != 'r' || str[len - 2] != 'e' || str[len - 3] != 'b'
-// 		|| str[len - 4] != '.')
-// 		return (ERR_EXTENSION_INVALID);
-// 	return (ERR_NONE);
-// }
